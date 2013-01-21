1a8369f5235e4ff90668bd6ec20b22242c2d6063
Shell M. Shrader <shell@shellware.com>
2013-01-20 21:32:35 -0500

    play store backup agent for shared preferences

---
+++ src/com/shellware/adaptronic/adaptive/tuner/MainActivityBackupAgentHelper.java
@@ -0,0 +1,35 @@
+/*
+ *   Copyright 2013 Shell M. Shrader
+ *   
+ *  Licensed under the Apache License, Version 2.0 (the "License");
+ *  you may not use this file except in compliance with the License.
+ *  You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *  Unless required by applicable law or agreed to in writing, software
+ *  distributed under the License is distributed on an "AS IS" BASIS,
+ *  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *  See the License for the specific language governing permissions and
+ *  limitations under the License.
+ *
+ */
+package com.shellware.adaptronic.adaptive.tuner;
+
+import android.app.backup.BackupAgentHelper;
+import android.app.backup.SharedPreferencesBackupHelper;
+
+public class MainActivityBackupAgentHelper extends BackupAgentHelper {
+    // The name of the SharedPreferences file
+    static final String PREFS = "com.shellware.adaptronic.adaptive.tuner_preferences";
+
+    // A key to uniquely identify the set of backup data
+    static final String PREFS_BACKUP_KEY = "prefs";
+
+    // Allocate a helper and add it to the backup agent
+    @Override
+    public void onCreate() {
+        SharedPreferencesBackupHelper helper = new SharedPreferencesBackupHelper(this, PREFS);
+        addHelper(PREFS_BACKUP_KEY, helper);
+    }
+}
