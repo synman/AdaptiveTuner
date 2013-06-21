f2f9e6838111892b45f348fcce70ae2ff0310a79
Shell M. Shrader <shell@shellware.com>
2013-06-21 15:56:28 -0400

    OPEN - task 6: Response discarded when using pl2303 adapter http://github.com/synman/AdaptiveTuner/issues/issue/6
    added gpl notice

--- src/com/shellware/adaptronic/adaptive/tuner/usb/PL2303SerialToUsbConnector.java
+++ src/com/shellware/adaptronic/adaptive/tuner/usb/PL2303SerialToUsbConnector.java
@@ -15,6 +15,22 @@ package com.shellware.adaptronic.adaptive.tuner.usb;
  *  See the License for the specific language governing permissions and
  *  limitations under the License.
  *
+ * PORTIONS COPYRIGHT
+ * 
+ * INDIserver is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * INDIserver is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with INDIserver.  If not, see <http://www.gnu.org/licenses/>.
+ *
+ * Copyright (C) 2012 Alexander Tuschen <atuschen75 at gmail dot com>
  */
 
 import java.io.IOException;
